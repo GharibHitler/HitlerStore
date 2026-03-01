56a719550171ff61d23f3456fe7c4dd1759edb1f
GharibHitler <0omargharib0@gmail.com>
2026-03-01 11:25:57 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyBQvpk9u4SfrZw-TgL6AWf8MeuwZuUmqVY",
+  authDomain:        "hitler-store-74008.firebaseapp.com",
+  projectId:         "hitler-store-74008",
+  storageBucket:     "hitler-store-74008.firebasestorage.app",
+  messagingSenderId: "1013000792954",
+  appId:             "1:1013000792954:web:f35f2dfd42338fee0244c3"
+};
+const STORE_CONFIG = {
+  name:          "Hitler Store",
+  adminPassword: "hs2025@admin",
+  adminKey:      "hs2025xOwner",
+  packages: [
+    { id:"uc60",   label:"60 UC",            price:55   },
+    { id:"uc325",  label:"325 UC",           price:240  },
+    { id:"uc385",  label:"385 UC",           price:290  },
+    { id:"uc660",  label:"660 UC",           price:450  },
+    { id:"uc1845", label:"1845 UC",          price:1200 },
+    { id:"uc3940", label:"3940 UC",          price:2300 },
+    { id:"uc8100", label:"8100 UC",          price:4450 },
+    { id:"season", label:"شحن السيزون (QR)", price:150  }
+  ]
+};
